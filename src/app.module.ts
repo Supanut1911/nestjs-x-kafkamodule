8b6ed8d93a5906c8cdb8f98cfcc9cde46f83c812
Supanut1911 <ynotmela@gmail.com>
2023-07-02 17:15:13 +0700

    add subscribe at handleSub

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-
+import { KafkaModule } from 'kafka/kafka.module';
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    KafkaModule.register({
+      clientId: 'nestjs-appY-kafkaModule',
+      brokers: [
+        process.env.KAFKA_BROKER_1,
+        process.env.KAFKA_BROKER_2,
+        process.env.KAFKA_BROKER_3,
+      ],
+      groupId: 'appYYYYYYYY.kafkaModule-consumer-group',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
+import { SubscribeTo } from 'kafka/kafka.decorator';
+import { KafkaPayload } from 'kafka/kafka.message';
+
+interface Subpayload {
+  x: number;
+  y: number;
+}
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  @SubscribeTo('need-sub-from-appY')
+  helloSubscriber(payload: Subpayload) {
+    console.log('[KAKFA-CONSUMER] Print message after receiving', payload);
   }
 }
